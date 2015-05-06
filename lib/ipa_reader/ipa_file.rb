f9e6497aa15ba33e183d647342da79a63f6f02bc
Garrit <contact@garritschaap.com>
2015-05-06 15:51:06 +0200

    Return data for PNG Icon

--- lib/ipa_reader/ipa_file.rb
+++ lib/ipa_reader/ipa_file.rb
@@ -16,27 +16,27 @@ module IpaReader
       cf_plist = CFPropertyList::List.new(:data => self.read_file(regex), :format => CFPropertyList::List::FORMAT_BINARY)
       self.plist = cf_plist.value.to_rb
     end
-    
+
     def version
       plist["CFBundleVersion"]
     end
-    
+
     def short_version
       plist["CFBundleShortVersionString"]
     end
-    
+
     def name
       plist["CFBundleDisplayName"]
     end
-    
+
     def target_os_version
       plist["DTPlatformVersion"].match(/[\d\.]*/)[0]
     end
-    
+
     def minimum_os_version
       plist["MinimumOSVersion"].match(/[\d\.]*/)[0]
     end
-    
+
     def url_schemes
       if plist["CFBundleURLTypes"] && plist["CFBundleURLTypes"][0] && plist["CFBundleURLTypes"][0]["CFBundleURLSchemes"]
         plist["CFBundleURLTypes"][0]["CFBundleURLSchemes"]
@@ -44,7 +44,7 @@ module IpaReader
         []
       end
     end
-    
+
     def icon_file
       if plist["CFBundleIconFiles"]
         data = read_file(Regexp.new("#{plist["CFBundleIconFiles"][0]}$"))
@@ -56,20 +56,21 @@ module IpaReader
       else
         nil
       end
+      data
     end
-    
+
     def mobile_provision_file
       read_file(/embedded\.mobileprovision$/)
     end
-    
+
     def bundle_identifier
       plist["CFBundleIdentifier"]
     end
-    
+
     def icon_prerendered
       plist["UIPrerenderedIcon"] == true
     end
-    
+
     def read_file(regex)
       file = nil
       Zip::File.foreach(self.file_path) { |f| file = f if f.name.match(regex) }
